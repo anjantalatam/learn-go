d6c79af611d8ec8672a5bb991094ea4dc6b6a1d0
Anjan Talatam <tspa2117@gmail.com>
2022-07-18 16:02:32 +0530

    17 - Slices and for loops

--- cards/main.go
+++ cards/main.go
@@ -1,5 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
-	printState()
+	// var cards1 = []string{"hi","hello"}
+	cards := []string{"Ace of diamonds", "King of Hearts"}
+	cards = append(cards, "Queen of Diamonds")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
