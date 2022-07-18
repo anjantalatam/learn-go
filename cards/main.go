899c30c40c1a6fd8af992e8356980da70aa6ec82
Anjan Talatam <tspa2117@gmail.com>
2022-07-18 22:40:54 +0530

    21 - Creating a new Deck

--- cards/main.go
+++ cards/main.go
@@ -1,15 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
-	// var cards1 = []string{"hi","hello"}
-	cards := deck{"Ace of diamonds", "King of Hearts"}
-	cards = append(cards, "Queen of Diamonds")
+	cards := newDeck()
 
 	cards.print()
-
-	var laptop laptopSize = 1.4
-	fmt.Println(laptop.getSizeOfLaptop())
 
 }
