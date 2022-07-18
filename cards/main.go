535d110232f077b0b82b3c29eef8d0213f3b4a6e
Anjan Talatam <tspa2117@gmail.com>
2022-07-18 17:16:55 +0530

    20 - Receiver functions

--- cards/main.go
+++ cards/main.go
@@ -1,9 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 	// var cards1 = []string{"hi","hello"}
 	cards := deck{"Ace of diamonds", "King of Hearts"}
 	cards = append(cards, "Queen of Diamonds")
 
 	cards.print()
+
+	var laptop laptopSize = 1.4
+	fmt.Println(laptop.getSizeOfLaptop())
+
 }
