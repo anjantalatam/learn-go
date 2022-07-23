e410a54d137701bb8c9df6525a5b023abc64e93c
Anjan Talatam <tspa2117@gmail.com>
2022-07-23 21:14:49 +0530

    35 - Testing file IO

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 
@@ -17,4 +20,24 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Four of Clubs" {
 		t.Errorf("Expected first card of Four of Clubs, but got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	fileName := "_decktesting"
+	e := os.Remove("fileName")
+
+	if e != nil {
+		t.Errorf("Removing file failed. File not found")
+	}
+	d := newDeck()
+
+	d.saveToFile(fileName)
+
+	loadedDeck := newDeckFromFile(fileName)
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(loadedDeck))
+	}
+
+	os.Remove(fileName)
 }
