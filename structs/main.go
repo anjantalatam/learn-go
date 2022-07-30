bb64041e201aa4a19d6aff6b4aee43598eb6580f
Anjan Talatam <tspa2117@gmail.com>
2022-07-30 16:45:46 +0530

    39 - Declaring Structs'

--- structs/main.go
+++ structs/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName  string
@@ -7,4 +9,7 @@ type person struct {
 
 func main() {
 
+	person1 := person{"Anjan", "Talatam"}
+	person2 := person{firstName: "Anjan", lastName: "Talatam"}
+	fmt.Println(person1, person2)
 }
