86b8946bd6c6655815d3c4f41dac8ca0c7cda40b
Anjan Talatam <tspa2117@gmail.com>
2022-07-30 17:06:24 +0530

    40 - Updating Struct Values

--- structs/main.go
+++ structs/main.go
@@ -9,8 +9,15 @@ type person struct {
 
 func main() {
 
-	person1 := person{"Anjan", "Talatam"}
-	persons := []string{"Anjan", "talatam"}
-	person2 := person{firstName: "Anjan", lastName: "Talatam"}
-	fmt.Println(person1, person2, persons)
+	// person1 := person{"Anjan", "Talatam"} //1
+	// person2 := person{firstName: "Anjan", lastName: "Talatam"} //2
+
+	var person3 person //3
+
+	person3.firstName = "Anjan"
+	person3.lastName = "Talatam"
+
+	fmt.Println(person3)
+	fmt.Printf("%+v", person3)
+
 }
