1e7fa4b7caf366c5f14d89b7b59138294c6c33bb
Anjan Talatam <tspa2117@gmail.com>
2022-07-31 12:39:49 +0530

    Quiz7-9

--- quiz/quiz7/main.go
+++ quiz/quiz7/main.go
@@ -1,17 +1,11 @@
-// What does the output of the code?
-// Bill
-// Alex
-// Empty String
+// What will the following program print out?
+
 package main
 
 import "fmt"
 
 func main() {
 	name := "Bill"
-	updateValue(name)
-	fmt.Println(name)
-}
 
-func updateValue(n string) {
-	n = "Alex"
+	fmt.Println(*&name)
 }
