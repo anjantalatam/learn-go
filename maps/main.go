d902af0e8f06d4f5ff2ee3e10ec9588918780ab8
Anjan Talatam <tspa2117@gmail.com>
2022-08-13 14:37:01 +0530

    50 - Manipulating Maps

--- maps/main.go
+++ maps/main.go
@@ -3,10 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#00ff00",
-		"blue":  "#0000ff",
-	}
+	// var colors map[string]string // 2
+
+	colors := make(map[int]string) // 3
+
+	// colors := map[string]string{ // 1
+	// 	"red":   "#ff0000",
+	// 	"green": "#00ff00",
+	// 	"blue":  "#0000ff",
+	// }
+
+	// Insert a key,value pair
+	colors[10] = "#ffffff"
+
+	// Delete a key,value pair
+	delete(colors, 10)
+
+	// 11 doesn't exist in colors: So colors is unaltered with this below line
+	delete(colors, 11)
+
 	fmt.Println(colors)
 }
