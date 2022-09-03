dfc37589adedec4df14ad7711166cabec4de70e4
Anjan Talatam <tspa2117@gmail.com>
2022-09-03 12:28:03 +0530

    58 - The HTTP Package

---
+++ http/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	resp, err := http.Get("http://google.com")
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(91)
+	}
+
+	fmt.Printf("%+v", resp)
+
+}
